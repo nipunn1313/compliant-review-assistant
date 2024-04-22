1ce07bf0acd18e225317d9d22d2d18a5c3018d62
Nipunn Koorapati <nipunn1313@gmail.com>
2024-04-22 16:48:26 -0700

    Add backfill script

--- convex/github.ts
+++ convex/github.ts
@@ -3,6 +3,7 @@ import { internal } from "./_generated/api";
 import { internalAction, internalMutation, query } from "./_generated/server";
 import { ConvexHttpClient } from "convex/browser";
 import { anyApi } from "convex/server";
+import { Octokit } from "@octokit/rest";
 
 export const updateLatestReleasePR = internalMutation({
   // Validators for arguments.
@@ -96,3 +97,82 @@ export const getLatestReleasePR = query({
       .first();
   },
 });
+
+export const backfill = internalAction({
+  args: {},
+  handler: async (ctx) => {
+    for (const row of await ctx.runMutation(internal.github.backfillFetcher)) {
+      if (row.requestor) {
+        continue;
+      }
+
+      const { author, firstApprover } = await fetchPRDetails(row.url);
+      await ctx.runMutation(internal.github.backfillWriter, {
+        id: row._id,
+        author,
+        firstApprover,
+      });
+    }
+  },
+});
+
+export const backfillFetcher = internalMutation({
+  args: {},
+  handler: async (ctx) => {
+    return await ctx.db
+      .query("release_prs")
+      .withIndex("by_creation_time")
+      .order("desc")
+      .collect();
+  },
+});
+
+export const backfillWriter = internalMutation({
+  args: {
+    id: v.id("release_prs"),
+    author: v.string(),
+    firstApprover: v.optional(v.string()),
+  },
+  handler: async (ctx, args) => {
+    return await ctx.db.patch(args.id, {
+      requestor: args.author,
+      approver: args.firstApprover,
+    });
+  },
+});
+
+async function fetchPRDetails(prUrl: string) {
+  const token = process.env.GITHUB_READ_TOKEN;
+  const octokit = new Octokit({ auth: token });
+
+  const match = prUrl.match(
+    /https:\/\/github\.com\/([^\/]+)\/([^\/]+)\/pull\/(\d+)/
+  );
+  if (!match) {
+    throw new Error("Invalid URL");
+  }
+
+  const [, owner, repo, pullNumber] = match;
+
+  // Fetch PR details
+  const { data: prData } = await octokit.pulls.get({
+    owner,
+    repo,
+    pull_number: +pullNumber,
+  });
+  const author = prData.user.login;
+
+  // Fetch PR reviews
+  const { data: reviews } = await octokit.pulls.listReviews({
+    owner,
+    repo,
+    pull_number: +pullNumber,
+  });
+  const approvedReviews = reviews.filter(
+    (review) => review.state === "APPROVED"
+  );
+  const firstApprover =
+    approvedReviews.length > 0 ? approvedReviews[0].user!.login : undefined;
+
+  return { author, firstApprover };
+}
